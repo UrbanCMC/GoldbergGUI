2b9ee795fb57c33ad83a450f9b47bdfd8dc052dc
Jeddunk <jedithug985@cock.li>
2021-01-09 22:51:44 +0100

    Added instructions for how to use ISecrets.cs

--- GoldbergGUI.Core/Utils/ISecrets.cs
+++ GoldbergGUI.Core/Utils/ISecrets.cs
@@ -1,3 +1,19 @@
+// Create a "Secrets" class, implement "ISecrets" interface and add your keys:
+/*
+using System;
+
+namespace GoldbergGUI.Core.Utils
+{
+    public class Secrets : ISecrets
+    {
+        public string SteamWebApiKey()
+        {
+            return "<ENTER STEAM WEB API KEY HERE>";
+        }
+    }
+}
+ */
+
 namespace GoldbergGUI.Core.Utils
 {
     public interface ISecrets
