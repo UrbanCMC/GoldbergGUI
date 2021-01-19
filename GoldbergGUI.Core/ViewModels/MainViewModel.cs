76f2d45f4531529fb63e0e1eafddd5586e9fc561
Jeddunk <jedithug985@cock.li>
2021-01-19 13:23:31 +0100

    Added UI elements for global options. Bigger minimal size for main window.

--- GoldbergGUI.Core/ViewModels/MainViewModel.cs
+++ GoldbergGUI.Core/ViewModels/MainViewModel.cs
@@ -264,6 +264,26 @@ namespace GoldbergGUI.Core.ViewModels
             }
         }
 
+        public class GlobalHelp
+        {
+            public static string Header => 
+                "Information\n";
+
+            public static string TextPreLink => 
+                "Usually these settings are saved under";
+
+            public static string Link => "%APPDATA%\\Goldberg SteamEmu Saves\\settings";
+
+            public static string TextPostLink => 
+                ", which makes these " +
+                "available for every game that uses the Goldberg Emulator. However, if you want to set specific settings " +
+                "for certain games (e.g. different language), you can remove the \"Global\" checkmark next to the option " +
+                "and then change it. If you want to remove that setting, just empty the field while \"Global\" is " +
+                "unchecked. (Not implemented yet!)";
+        }
+        
+        public static GlobalHelp G => new GlobalHelp();
+
         // COMMANDS //
 
         public IMvxCommand OpenFileCommand => new MvxAsyncCommand(OpenFile);
@@ -502,6 +522,22 @@ namespace GoldbergGUI.Core.ViewModels
                 }
             }
         });
+
+        public IMvxCommand OpenGlobalSettingsFolderCommand => new MvxCommand(OpenGlobalSettingsFolder);
+
+        private void OpenGlobalSettingsFolder()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                StatusText = "Can't open folder (Windows only)! Ready.";
+                return;
+            }
+
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "Goldberg SteamEmu Saves", "settings");
+            var start = Process.Start("explorer.exe", path);
+            start?.Dispose();
+        }
 
         // OTHER METHODS //
 
